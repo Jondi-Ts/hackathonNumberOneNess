a8b69eb4fff81279f61f11deb2271d4135b7491a
mimiisaak <mimiisaak2@gmail.com>
2021-11-28 13:46:04 +0200

    first commit

---
+++ src/main/java/Utilities/DDT/ManageDDT.java
@@ -0,0 +1,17 @@
+package Utilities.DDT;
+
+import Utilities.CommonOps;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import io.qameta.allure.Description;
+import org.testng.annotations.DataProvider;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ManageDDT extends CommonOps {
+
+}
---
+++ src/main/java/Utilities/Liseners.java
@@ -0,0 +1,50 @@
+package Utilities;
+
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+
+public class Liseners extends CommonOps {
+    public void onStart(ITestContext execution) {
+        System.out.println("---------------------- Starting Execution ------------------");
+    }
+
+    public void onFinish(ITestContext execution) {
+        System.out.println("---------------------- Ending Execution ------------------");
+    }
+
+    public void onTestStart(ITestResult test) {
+        System.out.println("---------------------- Test: " + test.getName() + " Started ------------------");
+    }
+
+    public void onTestSuccess(ITestResult test) {
+        System.out.println("---------------------- Test: " + test.getName() + " Passed ------------------");
+        try {
+            MonteScreenRecorder.stopRecord();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        File file = new File("./test-recordings/" + test.getName() + ".avi");
+        if (file.delete()) {
+            System.out.println("Recorded Screen Cast File Deleted Successfully");
+        } else {
+            System.out.println("Failed to Delete the Recorded Screen Cast File");
+        }
+    }
+
+    public void onTestFailure(ITestResult test) {
+        System.out.println("---------------------- Test " + test.getName() + " Failed ------------------");
+       // CommonOps.saveScreenshot();
+        try {
+            MonteScreenRecorder.stopRecord();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
