537bea687690247a597c72dbfc4fc2db58d3f008
Jondi-Ts <jondi.ts@gmail.com>
2021-11-29 14:46:06 +0200

    added jdbc class

--- src/main/java/workFlows/WorkFlowAppium.java
+++ src/main/java/workFlows/WorkFlowAppium.java
@@ -6,9 +6,9 @@ import com.google.common.util.concurrent.Uninterruptibles;
 import java.util.concurrent.TimeUnit;
 
 public class WorkFlowAppium extends CommonOps {
-    public static void createCalculation(String sendAmount, String sendyears, String sendPercent) {
+    public static void createCalculation(String sendAmount, String sendYears, String sendPercent) {
 
-        calculatorPage.sendKeys(sendAmount, sendyears, sendPercent);
+        calculatorPage.sendKeys(sendAmount, sendYears, sendPercent);
         calculatorPage.calculate();
         calculatorPage.save();
         Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
--- src/test/java/testCase/Test.java
+++ src/test/java/testCase/Test.java
@@ -1,4 +1,23 @@
 package testCase;
 
-public class test {
+import org.testng.annotations.*;
+import Utilities.CommonOps;
+import org.testng.Assert;
+import workFlows.WorkFlowAppium;
+
+public class Test extends CommonOps {
+
+    @org.testng.annotations.Test
+    public void test1() {
+
+        WorkFlowAppium.createCalculation("12000", "1", "1");
+        WorkFlowAppium.createCalculation("254345", "2", "1");
+        WorkFlowAppium.swipeAndCheckSave();
+    }
+
+    @org.testng.annotations.Test
+    public void test2() {
+
+        WorkFlowAppium.checkDelete();
+    }
 }
