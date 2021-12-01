8a1362abd90b6a1588b5937b9bce5dfeef50419d
Jondi-Ts <jondi.ts@gmail.com>
2021-12-01 15:44:55 +0200

    Api was added and chekded

--- src/main/java/pageObjects/toDoLIst/TodoPage.java
+++ src/main/java/pageObjects/toDoLIst/TodoPage.java
@@ -16,33 +16,30 @@ public class TodoPage extends Base {
     @FindBy(xpath = "//input[@placeholder='Create a task']")
     private WebElement input_toDo;
     @FindBy(xpath = "//div[@class='textWrapper_X9gil']/label")
-    public List<WebElement> list_of_todos;
+    private List<WebElement> list_of_todos;
     @FindBy(xpath = "//div[@class='view_2Ow90']//*[name()='svg'][@class='destroy_19w1q']")
     private List<WebElement> delete_btns;
 
+//
+//    @Step("create todo")
+//    public void makeTodo(String todoName) {
+//        input_toDo.sendKeys(todoName);
+//        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+//        input_toDo.sendKeys(Keys.ENTER);
+//    }
+//
+//
 
-    @Step("create todo")
-    public void makeTodo(String todoName) {
-        input_toDo.sendKeys(todoName);
-        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
-        input_toDo.sendKeys(Keys.ENTER);
+
+    public WebElement getInput_toDo() {
+        return input_toDo;
     }
 
-    @Step("edit todo name")
-    public void edit(int editTodoIndex) {
-        action = new Actions(driver);
-        action.moveToElement(list_of_todos.get(editTodoIndex)).doubleClick();
-        action.sendKeys(Keys.chord(Keys.CONTROL, "a")).sendKeys(Keys.BACK_SPACE).sendKeys(expectedEditedTitle);
-        action.sendKeys(Keys.ENTER).build().perform();
-
-
+    public List<WebElement> getList_of_todos() {
+        return list_of_todos;
     }
 
-    @Step("delete todo")
-    public void delete(int chooseIndexToDelete) {
-        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
-        delete_btns.get(chooseIndexToDelete).click();
-
+    public List<WebElement> getDelete_btns() {
+        return delete_btns;
     }
-
 }
--- src/main/java/workFlows/WorkFlowElectron.java
+++ src/main/java/workFlows/WorkFlowElectron.java
@@ -1,26 +1,60 @@
 package workFlows;
 
 import Utilities.CommonOps;
+import com.google.common.util.concurrent.Uninterruptibles;
+import extensions.UIActions;
 import io.qameta.allure.Step;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
 
 public class WorkFlowElectron extends CommonOps {
     @Step("creation of four new todos and getting their amount for checking ")
     public static int createFourTodos() {
-        for (int i = 1; i <= expectedCalculationsBeforeDelete; i++) {
-            todoPage.makeTodo("Todo " + i);
+        for (int i = 1; i <= expectedAmountofTodos; i++) {
+            UIActions.SendKeys(todoPage.getInput_toDo(), "todo " + i);
+            UIActions.SendKeys(todoPage.getInput_toDo(), Keys.ENTER);
         }
-        return todoPage.list_of_todos.size();
+        return todoPage.getList_of_todos().size();
     }
 
     @Step("editing todo name and geting text for cheking")
-    public static String edit(int chooseIndexOfTodo) {
-        todoPage.edit(chooseIndexOfTodo);
-        return todoPage.list_of_todos.get(chooseIndexOfTodo).getText();
+    public static String edit(int editTodoIndex) {
+        action = new Actions(driver);
+        action.moveToElement(todoPage.getList_of_todos().get(editTodoIndex)).doubleClick();
+        action.sendKeys(Keys.chord(Keys.CONTROL, "a")).sendKeys(Keys.BACK_SPACE).sendKeys(expectedEditedTitle);
+        action.sendKeys(Keys.ENTER).build().perform();
+
+
+        return todoPage.getList_of_todos().get(editTodoIndex).getText();
     }
 
-    @Step("deleting todo for and getting size of todos for checking delete function")
-    public static int deleteTodo(int chooseIndexOfTodo) {
-        todoPage.delete(chooseIndexOfTodo);
-        return todoPage.list_of_todos.size();
+    public static int deleteTodo() {
+        UIActions.Click(todoPage.getDelete_btns().get(0));
+        return todoPage.getDelete_btns().size();
     }
+//
+//    @Step("deleting todo for and getting size of todos for checking delete function")
+//    public static int deleteTodo(int chooseIndexOfTodo) {
+//        todoPage.delete(chooseIndexOfTodo);
+//        return todoPage.list_of_todos.size();
+//    }
+
+
+//    public void edit(int editTodoIndex) {
+//        action = new Actions(driver);
+//        action.moveToElement(list_of_todos.get(editTodoIndex)).doubleClick();
+//        action.sendKeys(Keys.chord(Keys.CONTROL, "a")).sendKeys(Keys.BACK_SPACE).sendKeys(expectedEditedTitle);
+//        action.sendKeys(Keys.ENTER).build().perform();
+//
+//
+//    }
+//
+//    @Step("delete
+//    public void delete(int chooseIndexToDelete) {
+//        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+//        delete_btns.get(chooseIndexToDelete).click();
+//
+//    }
 }
